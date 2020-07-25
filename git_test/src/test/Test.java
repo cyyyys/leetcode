6ba07a929d146d354497f28a8ce36003bcada19f
丛郁洋 <oncwnuLWXfaJ1AGKf4I_lqoUxUpU@git.weixin.qq.com>
2020-07-25 19:26:11 +0800

    测试

---
+++ git_test/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @Author: congyuyang
+ * @Date: 2020/7/25 下午7:17
+ *
+ *  1.将idea工程添加到本地仓库
+ *  2.使用idea克隆仓库并同步代码
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git_test");
+    }
+}
